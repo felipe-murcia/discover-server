865426b3bae572926a91116d6753c315264dca52
felipe-murcia <felipeperdomo32@hotmail.com>
2024-07-25 16:00:32 -0500

    prueba servidor en render

---
+++ app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"discoverco.co/server/configs"
+	"github.com/gin-gonic/gin"
+	//cors "github.com/itsjamie/gin-cors"
+)
+
+func init() {
+	configs.ConnectToDB()
+}
+
+func main() {
+
+	r := gin.Default()
+
+	// Apply the middleware to the router (works with groups too)
+	/*
+		r.Use(cors.Middleware(cors.Config{
+			Origins:         "*",
+			Methods:         "GET, PUT, POST, DELETE",
+			RequestHeaders:  "Origin, Authorization, Content-Type",
+			ExposedHeaders:  "",
+			MaxAge:          50 * time.Second,
+			Credentials:     false,
+			ValidateHeaders: false,
+		}))
+
+		routes.PersonRouter(r)*/
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello world from server Go.",
+		})
+	})
+	r.Run()
+}
